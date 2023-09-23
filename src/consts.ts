96fbc39f239738f7ebab5e469374598ef3e8fda9
cthoesen <cthoesen@gmail.com>
2023-09-23 13:05:47 -0400

    Header Added

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Knuckleheads Leagues';
+export const SITE_DESCRIPTION = 'A place to store my assets for use on MyFantasyLeague.com league sites.';
